9eb1c6c4ac4ac05eb2395e26bb9f6e036a6fc417
Trevor Karl <90585553+trevor020601@users.noreply.github.com>
2022-12-29 15:19:11 -0600

    Add files via upload

---
+++ p2_C00445623_C00441253/p2client_C00441253_C00445623/Program.cs
@@ -0,0 +1,206 @@
+﻿/*
+Author:     Rodney Harris and Trevor Karl
+CLID:       C00445623 and C00441253 
+Class:      CMPS 358
+Assignment: Project #2
+Due Date:   11:59 P.M. October 18, 2022
+Description: Client that connects to the server to chat with other clients.
+*/
+
+using System.IO;
+using static System.Environment;
+using static System.IO.Path;
+using System.Security.Cryptography;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+Console.WriteLine("Client:");
+Client();
+
+/*
+ * Client
+ *
+ * Output: Strings to the server that client types to make requests to the server
+*/
+static void Client()
+{
+    
+    using var connectionToServer = new TcpClient("localhost", 8081);
+    using var theServer = connectionToServer.GetStream();
+
+    var br = new BinaryReader(theServer);
+    var bw = new BinaryWriter(theServer);
+    
+    new StreamFromServerInput(br);
+
+    Console.WriteLine("1-Register a new user account");
+    Console.WriteLine("2-Send a message to a user account");
+    Console.WriteLine("3-Get all messages for a user account");
+    Console.WriteLine("0-Exit");
+
+    var input = Console.ReadLine();
+    var newCommand = true;
+    var readingMenu = false;
+    var name = " ";
+
+    try
+    {
+        while (true)
+        {
+            if(readingMenu)
+            {
+                Console.WriteLine("1-Register a new user account");
+                Console.WriteLine("2-Send a message to a user account");
+                Console.WriteLine("3-Get all messages for a user account");
+                Console.WriteLine("0-Exit");
+
+                input = Console.ReadLine();
+                if(input == "1" || input == "2" || input == "3" || input == "0")
+                {
+                    readingMenu = false;
+                    newCommand = true;
+                }
+            }
+            if(input == "1" && newCommand) {
+                newCommand = false;
+
+                Console.WriteLine("Enter your username:");
+
+                name = Console.ReadLine();
+
+                using (var rsa = new RSACryptoServiceProvider())
+                {
+                    File.WriteAllText(Combine(CurrentDirectory, name + "PublicKeyOnly.xml"), rsa.ToXmlString (false));
+                    File.WriteAllText(Combine(CurrentDirectory, name + "PrivateKeyOnly.xml"), rsa.ToXmlString(true));
+                }
+
+                var message = name + "~" + File.ReadAllText(Combine(CurrentDirectory, name +"PublicKeyOnly.xml"));
+                bw.Write(message);
+                bw.Flush();
+
+                readingMenu = true;
+
+            }
+            else if(input == "2" && newCommand) {
+                newCommand = false;
+
+                Console.WriteLine("Enter a user to receive messages: ");
+                var receiver = Console.ReadLine();
+                Console.WriteLine("Enter a message: ");
+                var message = Console.ReadLine();
+
+                bw.Write("*Receiver:" + receiver + "*,*" + message);
+                bw.Flush();
+
+                readingMenu = true;
+            }
+            else if(input == "3" && newCommand) {
+                newCommand = false;
+
+
+                Console.WriteLine("Enter a user to get messages: ");
+                var getter = Console.ReadLine();
+
+                bw.Write("*Getter:" + getter);
+
+                bw.Flush();
+
+                readingMenu = true;
+            }
+            else if(input == "0" && newCommand){
+                newCommand = false;
+
+                connectionToServer.Close();
+                
+            }
+
+        }
+    }
+    catch
+    {
+        bw.Close();
+    }
+}
+
+/*
+ * StramFromServerInput
+ *
+ * Class for receiving input from the server for client interactions
+*/
+internal class StreamFromServerInput
+{
+    /*
+     * StreamFromServerInput
+     *
+     * Runs a thread for InputLoop to constantly listen for client input
+     *
+     * Input: br (BinaryReader for receiving client input)
+    */
+    public StreamFromServerInput(BinaryReader br)
+    {
+        Task.Run(() => InputLoop(br)); 
+    }
+
+    static string lastMD = "";
+
+    /*
+     * PassByteString
+     *
+     * Decrypts the encrypted message sent from the server for user retrieval
+     *
+     * Input: getter (name of the receiver), byteString (encrypted message from server)
+     * Output: decrypted message
+    */
+    static void PassByteString(string getter, string byteString) {
+
+        string publicPrivate = File.ReadAllText(Combine(CurrentDirectory,getter + "PrivateKeyOnly.xml"));
+
+        byte[] dataout = Convert.FromBase64String(byteString);
+
+        string messageDecrypted;
+
+        byte[] decrypted;
+        using (var rsaPublicPrivate = new RSACryptoServiceProvider())
+        {
+            rsaPublicPrivate.FromXmlString (publicPrivate);
+            decrypted = rsaPublicPrivate.Decrypt(dataout, true);
+            messageDecrypted = Encoding.UTF8.GetString(decrypted);
+        }
+
+        lastMD = messageDecrypted;
+
+        if(lastMD == messageDecrypted)
+            Console.WriteLine("Message: " + messageDecrypted);
+    
+    }
+
+    /*
+     * InputLoop
+     *
+     * Asynchronous method that listens for client input
+     *
+     * Input: br (BinaryReader for receiving client input)
+     * Output: Passes receiver name and message to PassByteString
+     * 
+    */
+    private async void InputLoop(BinaryReader br) {
+        try
+        {
+            while (true)
+            {
+                var incoming = br.ReadString();
+
+                if(incoming.Contains("::")) {
+                    var incomingSplit = incoming.Split("::");
+                    PassByteString(incomingSplit[0], incomingSplit[1]);
+                }
+
+            }
+        }
+        catch
+        {
+            br.Close();
+        }
+    }
+}
---
+++ p2_C00445623_C00441253/p2server_C00441253_C00445623/Program.cs
@@ -0,0 +1,158 @@
+﻿/*
+Author:     Rodney Harris and Trevor Karl
+CLID:       C00445623 and C00441253 
+Class:      CMPS 358
+Assignment: Project #2
+Due Date:   11:59 P.M. October 18, 2022
+Description: Server that can support multiple clients chatting with each other.
+*/
+
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using static System.Environment;
+using static System.IO.Path;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
+
+
+var clientList = new List<StreamFromClientInput>();
+List<Tuple<string, string>> clientListPseudoDB = new List<Tuple<string, string>>();
+List<Tuple<string, string>> messagesPseudoDB = new List<Tuple<string, string>>();
+XmlDocument keyAccess = new XmlDocument();
+
+var listen = new TcpListener(IPAddress.Any, 8081);
+listen.Start();
+Console.WriteLine("Listening for client on 8081 ...");
+
+while (true)
+{
+    var clientConnect = listen.AcceptTcpClient();
+    var theClient = clientConnect.GetStream();
+
+    var bw = new BinaryWriter(theClient);
+    var br = new BinaryReader(theClient);
+
+    var sfcInput = new StreamFromClientInput(br, bw, clientList, clientListPseudoDB, messagesPseudoDB);
+    lock (clientList)
+    {
+        clientList.Add(sfcInput);
+    }
+}
+
+/*
+ * StreamFromClientInput
+ *
+ * Class for receiving the String inputs from the client(s) and handling the requests
+*/
+internal class StreamFromClientInput
+{
+    private readonly BinaryWriter bw;
+
+    /*
+     * StreamFromClientInput
+     *
+     * Runs a thread to listen for messages from the client 
+    */
+    public StreamFromClientInput(BinaryReader br, BinaryWriter bw, List<StreamFromClientInput> list, List<Tuple<string,string>> pseudoDB, List<Tuple<string,string>> messageDB)
+    {
+        this.bw = bw;
+        Task.Run(() =>  InputLoop(br, list, pseudoDB, messageDB));
+    }
+
+    /*
+     * InputLoop
+     *
+     * Receives messages from the clients that contain requests for the server to handle 
+     *
+     * Input: br (BinaryReader for receiving client input), list (list of clients), 
+     *        pseudoDB (Stores client names and public keys), messageDB (stores messages from clients)
+     * 
+     * Output: Encrypted messages 
+    */
+    private async Task InputLoop(BinaryReader br, List<StreamFromClientInput> list, List<Tuple<string,string>> pseudoDB, List<Tuple<string,string>> messageDB)
+    {
+        try
+        {
+            while (true)
+            {
+                //waits for input from client
+                var incoming = br.ReadString();
+                foreach (var c in list)
+                    try
+                    {
+                        c.bw.Write(incoming);
+
+                        if(incoming.Contains("~") && incoming.Contains("<RSAKeyValue>"))
+                        {
+                            var userTuple = incoming.Split("~");
+
+                            pseudoDB.Add(new Tuple<string,string>(userTuple[0], userTuple[1]));
+
+                            Console.WriteLine(pseudoDB.Last());
+                        }
+                        
+                        // Used when a message is sent from client side to be encrypted and stored for when the receiving
+                        // user wants to see their messages.
+                        
+                        if(incoming.Contains("*Receiver:")) {
+                            var newMessage = incoming.Replace("*Receiver:", "");
+                            var newMessageArr = newMessage.Split("*,*");
+
+                            foreach(Tuple<string,string> uTuple in pseudoDB)
+                            {
+                                if(uTuple.Item1 == newMessageArr[0])
+                                {
+                                    byte[] datain = Encoding.UTF8.GetBytes (newMessageArr[1]);
+                                    string publicKeyOnly = uTuple.Item2;
+                                    byte[] encrypted;
+                                    using (var rsaPublicOnly = new RSACryptoServiceProvider())
+                                    {
+                                        rsaPublicOnly.FromXmlString (publicKeyOnly);
+                                        encrypted = rsaPublicOnly.Encrypt (datain, true);
+                                    }
+
+                                    var encryptedMessage = Convert.ToBase64String(encrypted);
+
+                                    messageDB.Add(new Tuple<string,string>(uTuple.Item1, uTuple.Item1 + "::" + encryptedMessage)); 
+                                }
+                                else
+                                {
+
+                                }
+                            }        
+                        }
+
+                        //Checks when someone attempt to receive messages on the client side
+
+                        if(incoming.Contains("*Getter:")) {
+                            var gettingMessage = incoming.Replace("*Getter:", "");
+
+                            foreach(Tuple<string,string> uTuple in messageDB)
+                            {
+                                if(uTuple.Item1 == gettingMessage)
+                                {
+                                    
+                                    bw.Write(uTuple.Item2);
+                                    bw.Flush();
+                                    
+                                }
+                            }
+
+                        }
+
+                    }
+                    catch
+                    {
+                        c.bw.Close();
+                    }
+            }
+        }
+        catch
+        {
+            br.Close();
+        }
+    }
+}
+
